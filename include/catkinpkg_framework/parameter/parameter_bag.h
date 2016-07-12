6baf703d3edf4b7b0524dfb998ece6f2812b3893
magrimm <marius.grimm93@web.de>
2016-07-12 14:47:04 +0200

    added doxy comments

--- include/catkinpkg_framework/parameter/parameter_bag.h
+++ include/catkinpkg_framework/parameter/parameter_bag.h
@@ -9,20 +9,38 @@
 
 namespace framework {
 
+/**
+ * \brief Struct to store all necessary parameters for the project with
+ *        substructure of parameter bags for other modules.
+ * @author Marius Grimm (marius.grimm93@web.de)
+ * @date April, 2016
+ */
 struct ParameterBag {
-  // Parameter specific to parameter bag
+  /** Node name of the tracking module. */
   std::string node_name;
 
+  /** Subscribed rostopic for the image. */
   std::string sub_rostopic_img;
+
+  /** Queue size for the image rostopic subscriber. */
   int queue_size_sub_img;
 
+  /** Subscribed rostopic for the camera info. */
   std::string sub_rostopic_cam;
+
+  /** Queue size for the camera info rostopic subscriber. */
   int queue_size_sub_cam;
 
+  /** Published rostopic for the objects. */
   std::string pub_rostopic_objects;
+
+  /** Queue size for the object rostopic publisher. */
   int queue_size_pub_objects;
 
+  /** Published rostopic for the image. */
   std::string pub_rostopic_img;
+
+  /** Queue size for the image rostopic publisher. */
   int queue_size_pub_img;
 };
 
